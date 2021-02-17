ea59c37261931d1f1cbe55b2c35b700bfb674715
xun2022 <69636714+xun2022@users.noreply.github.com>
2021-02-17 14:53:55 -0500

    Update dbhandler.py

--- A1/app/dbhandler.py
+++ A1/app/dbhandler.py
@@ -38,8 +38,7 @@ def teardown_db(exception):
 s3 = boto3.client(
     "s3",
     aws_access_key_id='AWS_ACCESS_KEY',
-    aws_secret_access_key='AWS_SECRET_ACCESS_KEY',
-    aws_session_token='AWS_SESSION_TOKEN'
+    aws_secret_access_key='AWS_SECRET_ACCESS_KEY'
 )
 
-BUCKET_NAME = 'a1container'
+BUCKET_NAME = 'a1-db'
