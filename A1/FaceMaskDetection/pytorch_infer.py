f6cf12dab49b70f3fb4f6944fa99c7c867526f23
suzhe96 <oliversu96@yahoo.com>
2021-02-22 11:51:35 +0800

    Zhe: change face color code

--- A1/FaceMaskDetection/pytorch_infer.py
+++ A1/FaceMaskDetection/pytorch_infer.py
@@ -87,7 +87,7 @@ def inference(image,
                 total_masked += 1
                 color = (0, 255, 0)
             else:
-                color = (255, 0, 0)
+                color = (0, 0, 255)
             cv2.rectangle(image, (xmin, ymin), (xmax, ymax), color, 2)
             cv2.putText(image, "%s: %.2f" % (id2class[class_id], conf), (xmin + 2, ymin - 2),
                         cv2.FONT_HERSHEY_SIMPLEX, 0.8, color)
