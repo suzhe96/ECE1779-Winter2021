95b83bc047a4686bf7f5bcdbe86f3844ddae6052
suzhe96 <oliversu96@yahoo.com>
2021-03-11 14:36:39 +0800

    Zhe: add aws helper functions

---
+++ A2/app/awsconfig.py
@@ -0,0 +1,53 @@
+AWS_CREDENTIALS_CONFIG = {
+    'request'  : 'http://169.254.169.254/latest/meta-data/iam/security-credentials/{}',
+    'iam_role' : 'ECE1779-ADMIN'
+}
+
+AWS_ELB_CONFIG = {
+    'lb' : 'app/ece1779-lb-group-zhe/70e467fa27db17ed',
+    'lbARN' : 'arn:aws:elasticloadbalancing:us-east-1:905405143286:loadbalancer/app/ece1779-lb-group-zhe/70e467fa27db17ed',
+    'lbDNS' : 'ece1779-lb-group-zhe-1258727599.us-east-1.elb.amazonaws.com'
+}
+
+AWS_TARGET_GROUP_CONFIG = {
+    'targetgroup' : 'targetgroup/ece1779-lb-target-group-zhe/674e2a3500167196',
+    'targetgroupARN' : 'arn:aws:elasticloadbalancing:us-east-1:905405143286:targetgroup/ece1779-lb-target-group-zhe/674e2a3500167196'
+}
+
+AWS_GENERAL_CONFIG = {
+    'region' : 'us-east-1',
+    'elb_service' : 'elbv2',
+    'ec2_service' : 'ec2',
+    'cloudwatch_service' : 'cloudwatch'
+}
+
+AWS_CLOUDWATCH_CONFIG = {
+    'elb_namespace' : 'AWS/ApplicationELB',
+    'ec2_namespace' : 'AWS/EC2',
+    'http_namespace' : 'HTTP/Requests',
+    'CPU_metrics' : 'CPUUtilization',
+    'healthy_metrics' : 'HealthyHostCount',
+    'http_metrics' : 'HTTPRequestsMetrics',
+    'statistics_avg' : 'average',
+    'statisitcs_max' : 'Maximum',
+}
+
+AWS_EC2_STATUS_PENDING = 'pending'
+AWS_EC2_STATUS_RUNNING = 'running'
+AWS_EC2_STATUS_STOPPING = 'stopping'
+AWS_EC2_STATUS_DOWN = 'terminated'
+
+AWS_EC2_SCALING_UP = 'up'
+AWS_EC2_SCALING_DOWN = 'down'
+
+AWS_EC2_NUM_MAX = 8
+AWS_EC2_NUM_MIN = 1
+
+AWS_OK = 0
+AWS_ERROR_EC2_NUM_EXCEED_MAX = 1501
+AWS_ERROR_EC2_NUM_BELOW_MIN = 1502
+
+AWS_ERROR_MSG = {
+   AWS_ERROR_EC2_NUM_EXCEED_MAX : 'EC2 instance number maximum reached',
+   AWS_ERROR_EC2_NUM_BELOW_MIN : 'EC2 instance number minimum reached'
+}
---
+++ A2/app/awshandler.py
@@ -0,0 +1,62 @@
+import requests
+import json
+import mysql.connector
+from flask import g
+from app import a2 
+from app.awsconfig import *
+import boto3, botocore
+
+
+
+
+def get_aws_credentials():
+    aws_response = requests.get(AWS_CREDENTIALS_CONFIG['request'].format(AWS_CREDENTIALS_CONFIG['iam_role']))
+    aws_response_json = json.loads(aws_response.content.decode())
+    return {'aws_access_key' : aws_response_json['AccessKeyId'],
+            'aws_secret_access_key' : aws_response_json['SecretAccessKey'],
+            'aws_session_token' : aws_response_json['Token']}
+
+
+def get_boto3_session():
+    aws_credentials = get_aws_credentials()
+    session = getattr(g, '_boto3_session', None)
+    if session is None:
+        session = g._boto3_session = boto3.Session(aws_access_key_id=aws_credentials['aws_access_key'],
+                                                   aws_secret_access_key=aws_credentials['aws_secret_access_key'],
+                                                   aws_session_token=aws_credentials['aws_session_token'],
+                                                   region_name=AWS_GENERAL_CONFIG['region'])
+    return session
+
+
+def get_elb():
+    elb = getattr(g, '_boto3_elb', None)
+    if elb is None:
+        session = get_boto3_session() 
+        elb = g._boto3_elb = session.client(AWS_GENERAL_CONFIG['elb_service'],
+                                            region_name=AWS_GENERAL_CONFIG['region'])
+    return elb
+
+
+def get_ec2():
+    ec2 = getattr(g, '_boto3_ec2', None)
+    if ec2 is None:
+        session = get_boto3_session() 
+        ec2 = g._boto3_ec2 = session.client(AWS_GENERAL_CONFIG['ec2_service'],
+                                            region_name=AWS_GENERAL_CONFIG['region'])
+    return ec2
+
+def get_ec2_resource():
+    ec2_resource = getattr(g, '_boto3_ec2_resource', None)
+    if ec2_resource is None:
+        session = get_boto3_session() 
+        ec2_resource = g._boto3_ec2_resource = session.resource(AWS_GENERAL_CONFIG['ec2_service'])
+    return ec2_resource
+
+
+def get_cloudwatch():
+    cloudwatch = getattr(g, '_boto3_cloudwatch', None)
+    if cloudwatch is None:
+        session = get_boto3_session() 
+        cloudwatch = g._boto3_cloudwatch = session.client(AWS_GENERAL_CONFIG['cloudwatch_service'])
+    return cloudwatch
+
