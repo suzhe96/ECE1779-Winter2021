b1cb031345a73577feabe875225f0d1094f4006d
suzhe96 <oliversu96@yahoo.com>
2021-03-13 17:43:41 +0800

    Zhe: fix error

--- A1/start.sh
+++ A1/start.sh
@@ -1,1 +1,1 @@
-gunicorn --bind 0.0.0.0:5000 --workers=1 --threads=2 --access-logfile access.log --error-logfile error.log app:app
+gunicorn --bind 0.0.0.0:5000 --workers=1 --threads=2 --access-logfile access.log --error-logfile error.log --chdir /home/ubuntu/Desktop/ECE1779-Winter2021/A1 app:app
