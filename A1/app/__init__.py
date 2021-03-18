27980818062075ceef3fb383d03d5732cf268184
suzhe96 <oliversu96@yahoo.com>
2021-03-18 15:13:26 +0800

    Zhe: fix A1 circular import error

--- A1/app/__init__.py
+++ A1/app/__init__.py
@@ -8,7 +8,6 @@ from threading import Thread
 
 app = Flask(__name__)
 
-from app import home, user, models, detector
 from app import awsconfig, awshandler, awsworker
 
 app.config.from_object(Config)
@@ -33,6 +32,7 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 
+from app import home, user, models, detector
 http_request_backgroud_task = Thread(target=awsworker.publish_http_request_cb)
 http_request_backgroud_task.daemon = True
 http_request_backgroud_task.start()
